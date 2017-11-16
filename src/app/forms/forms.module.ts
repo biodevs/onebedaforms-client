006e21551bf6ee530385cbe21d742eed1f735fec
Marcio <marcio@mail.com>
2017-11-16 00:06:42 -0200

    adicionado component html in login e register, conexao com api;

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormListComponent } from './form-list/form-list.component';
 import { ActiveComponent } from './shared/active/active.component';
+import { FormEditComponent } from './form-edit/form-edit.component';
+import { FormNewComponent } from './form-new/form-new.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FormListComponent, ActiveComponent]
+  declarations: [FormListComponent, ActiveComponent, FormEditComponent, FormNewComponent]
 })
 export class FormsModule { }
